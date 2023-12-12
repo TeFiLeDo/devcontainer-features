031060cc4a1259c0c2757676780d2309986cd31d
Adrian Wannenmacher <tfld@tfld.dev>
2023-12-12 02:57:55 +0100

    output download url

--- src/sqlx/install.sh
+++ src/sqlx/install.sh
@@ -1,5 +1,6 @@
 echo "Activating feature 'sqlx'"
 
+echo downloading: https://github.com/cargo-bins/cargo-quickinstall/releases/download/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 curl -OL https://github.com/cargo-bins/cargo-quickinstall/releases/download/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 tar -xzf sqlx-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 cp sqlx /usr/local/bin/

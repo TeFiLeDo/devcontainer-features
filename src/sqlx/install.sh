dd9b452704a925a87138a3b96fcc6bfeec721c18
Adrian Wannenmacher <tfld@tfld.dev>
2023-12-12 02:53:01 +0100

    make curl not quiet

--- src/sqlx/install.sh
+++ src/sqlx/install.sh
@@ -1,6 +1,6 @@
 echo "Activating feature 'sqlx'"
 
-curl -sOL https://github.com/cargo-bins/cargo-quickinstall/releases/download/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
+curl -OL https://github.com/cargo-bins/cargo-quickinstall/releases/download/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu/sqlx-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 tar -xzf sqlx-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 cp sqlx /usr/local/bin/
 rm sqlx cargo-sqlx sqlx-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
